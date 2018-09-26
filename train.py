747044d63a36b60a0de5f0bbe58e405a076ac26f
Joeky Zhan <jj16180339887@gmail.com>
2018-09-26 11:21:56 +0800

    Finish train.py
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- train.py
+++ train.py
@@ -1,41 +1,83 @@
-from tensorflow.python.client import device_lib
-
 import re
 import numpy as np
+from tensorflow.python.client import device_lib
+from tensorflow.keras import utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras import optimizers
 
-TRAINFILE = "./train.csv"
+csvfile   = "./train.csv"
+modelfile = "./model.h5"
+
 images = []
-imagesNp = np.array([])
+x_train = np.array([])
+y_train = np.array([])
 
-imageH = 48
-imageW = 48
+imgW, imgH = 48, 48
+epochs = 80
+batch_size = 480
+classes_num = 7  # 7 different expressions
 
-class image():
-    def __init__(self, tag, pixels):
-        self.tag = tag # int
-        self.pixels = pixels # numpy 2d array
-        # self.train_x = np.array(pixels, imageH, imageW, 1)
+model = Sequential()
 
 def openTrainFile():
-    with open(TRAINFILE) as f:
+    global x_train
+    global y_train
+    with open(csvfile) as f:
         content = f.readlines()
     content.pop(0)
     content = [x.strip() for x in content]
+    tags = []
     for l in content:
         tag = int(re.search(r'^\d+', l).group())
         pixels = [int(p) for p in re.sub(r'^\d+,', '', l).split()]
 
         images.append(
             # np.reshape(np.array(pixels), (-1, imageW))
-            np.array(pixels).reshape(imageW, imageH, 1)
+            np.array(pixels).reshape(imgW, imgH, 1)
         )
+        tags.append(tag)
 
-    imagesNp = np.array(images)
-    print(images[0][0][2][0])
-    print(imagesNp.shape)
+    x_train = np.array(images)
+    y_train = utils.to_categorical(np.array(tags))
 
+def train():
+    global x_train
+    global y_train
+    model.add(Conv2D(32,
+                     kernel_size=(3, 3),
+                     activation='relu',
+                     padding="same",
+                     input_shape=(imgW, imgH, 1)))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(classes_num, activation='softmax'))
+
+    model.summary()
+    print()
+
+    model.compile(loss=categorical_crossentropy,
+                  optimizer=optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x=x_train,
+              y=y_train,
+              batch_size=batch_size,
+              verbose=2,
+              epochs=epochs,
+              validation_split=0.2)
+
+    model.save(filepath=modelfile)
 
 if __name__ == '__main__':
-    # print(device_lib.list_local_devices())
+    print(device_lib.list_local_devices())
     openTrainFile()
-
+    train()
